820a755bec9ac43c3922ff43cf7de8e39171510f
Alexandra Sivira <kerensivira@gmail.com>
2022-02-12 16:31:58 -0500

    navbar

--- src/Components/BarraDeNavegacion/Navbar.js
+++ src/Components/BarraDeNavegacion/Navbar.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Navbar } from 'react-bootstrap';
+import { Nav, Navbar, NavbarBrand } from 'react-bootstrap';
 
 export function BarraDeNavegacion() {
 
     return(
 
+      <header>
       <div className='BarraDeNavegacion'>
-        <Navbar bg='dark'>
+        <Navbar bg='dark' variant='dark' sticky='top' expand='lg'>   
+<Navbar.Brand>
+  <img 
+  src ={"https://res.cloudinary.com/alexa01020304/image/upload/v1644454466/logo-blockBuster_aqx7hs.png"}
+  width= "100px" height="60px"
+  />
+</Navbar.Brand>
 
+<Navbar.Toggle/>
+
+<Navbar.Collapse>
+<Nav>
+  <Nav.Link to ='../BarraDeNavegacion/pages/Todas.js'>Todas</Nav.Link>
+  <Nav.Link to ='../BarraDeNavegacion/pages/MásValoradas.js'>Más Valoradas</Nav.Link>
+  <Nav.Link to ='../BarraDeNavegacion/pages/MenosValoradas.js'>Menos Valoradas</Nav.Link>
+</Nav>
+</Navbar.Collapse>
         </Navbar>
       </div>
-    
+      </header>
             
          
     )
